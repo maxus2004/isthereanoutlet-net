8fb7de804fa364b01f6d185be5504e47c52c1d50
Maksim Doroshin <m1doroshin@email.com>
2025-08-21 15:46:45 +0300

    added vk bridge

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 "use client"
 
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import bridge from '@vkontakte/vk-bridge';
 
 const MapDisplay = dynamic(() => import("./components/Map"), { ssr: false });
 
 export default function Home() {
+    useEffect(() => {
+        bridge.send('VKWebAppInit')
+        .then((data) => { 
+            if (data.result) {
+            // Приложение инициализировано
+            } else {
+            // Ошибка
+            }
+        })
+        .catch((error) => {
+            // Ошибка
+            console.log(error);
+        });
+    }, []);
     return (
         <div className="font-montserrat">
             <MapDisplay />
